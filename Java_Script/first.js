0d0a20ed1586daf18cd2aa77d82cafa42aac80de
Nagesh Pandey <i.am.nageshpandey@gmail.com>
2024-01-06 20:16:10 +0530

    commit

---
+++ Java_Script/first.js
@@ -0,0 +1,22 @@
+
+//printing method in javascript
+console.log("hello");
+console.log("how are you");
+
+//function in javascript
+function myfunction(){
+    console.log("hello");
+    console.log("how are you");
+}
+myfunction();
+
+//mathematical operation in javascript
+let x, y;
+x = 20;
+y = 5;
+console.log(x + y);  //addition
+console.log(x - y);  //subtraction
+console.log(x * y);  //multiplication
+console.log(x / y);  //division
+console.log(x % y);  //ramainder
+
